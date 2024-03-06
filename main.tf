c8f25d84b562b7ba8e364c6e70f4f37e40196489
dkool <dkool@dkools-MacBook-Pro.local>
2024-03-06 15:09:54 +0100

    feat: cleanups

--- main.tf
+++ main.tf
@@ -122,10 +122,22 @@ resource "azurerm_mssql_database" "database" {
 }
 
 resource "azurerm_user_assigned_identity" "identity" {
-  for_each = contains(["UserAssigned", "SystemAssigned, UserAssigned"], try(var.instance.identity.type, "")) ? { "identity" = var.instance.identity } : {}
+  for_each = contains(
+    ["UserAssigned", "SystemAssigned, UserAssigned"], try(var.instance.identity.type, "")
+  ) ? { (var.instance.name) = {} } : {}
 
-  name                = try(each.value.name, "uai-${var.instance.name}")
+
+  name                = try(var.instance.identity.name, "uai-${var.instance.name}")
   resource_group_name = coalesce(lookup(var.instance, "resourcegroup", null), var.resourcegroup)
   location            = coalesce(lookup(var.instance, "location", null), var.location)
-  tags                = try(each.value.tags, var.tags, null)
+  tags                = try(var.instance.identity.tags, var.tags, null)
 }
+
+#resource "azurerm_user_assigned_identity" "identity" {
+  #for_each = contains(["UserAssigned", "SystemAssigned, UserAssigned"], try(var.instance.identity.type, "")) ? { "identity" = var.instance.identity } : {}
+
+  #name                = try(each.value.name, "uai-${var.instance.name}")
+  #resource_group_name = coalesce(lookup(var.instance, "resourcegroup", null), var.resourcegroup)
+  #location            = coalesce(lookup(var.instance, "location", null), var.location)
+  #tags                = try(each.value.tags, var.tags, null)
+#}
