53a1b3f0ba3aee48598e1a5eaa2ace1c7847845f
mark.smit <mark.smit@cloudnation.nl>
2025-04-09 14:10:35 +0200

    feat: add schema validation items for sql

--- main.tf
+++ main.tf
@@ -7,8 +7,11 @@ resource "azurerm_mssql_server" "sql" {
   public_network_access_enabled                = try(var.instance.public_network_access_enabled, true)
   primary_user_assigned_identity_id            = try(var.instance.primary_user_assigned_identity_id, null)
   administrator_login                          = try(var.instance.administrator_login, "adminLogin")
-  administrator_login_password                 = var.instance.administrator_login_password
+  administrator_login_password                 = try(var.instance.administrator_login_password, null)
+  administrator_login_password_wo              = try(var.instance.administrator_login_password_wo, null)
+  administrator_login_password_wo_version      = try(var.instance.administrator_login_password_wo_version, null)
   connection_policy                            = try(var.instance.connection_policy, "Default")
+  express_vulnerability_assessment_enabled     = try(var.instance.express_vulnerability_assessment_enabled, false)
   minimum_tls_version                          = try(var.instance.minimum_tls_version, "1.2")
   outbound_network_restriction_enabled         = try(var.instance.outbound_network_restriction_enabled, false)
   transparent_data_encryption_key_vault_key_id = try(var.instance.transparent_data_encryption_key_vault_key_id, null)
@@ -31,6 +34,7 @@ resource "azurerm_mssql_server" "sql" {
     content {
       login_username              = try(azuread_administrator.value.login_username, null)
       object_id                   = try(azuread_administrator.value.object_id, null)
+      tenant_id                   = try(azuread_administrator.value.tenant_id, null)
       azuread_authentication_only = try(azuread_administrator.value.azuread_authentication_only, null)
     }
   }
@@ -102,7 +106,7 @@ resource "azurerm_mssql_database" "database" {
   sku_name                                                   = each.value.sku != "ElasticPool" ? each.value.sku : null
   elastic_pool_id                                            = try(azurerm_mssql_elasticpool.elasticpool[each.value.elasticpool].id, null)
   min_capacity                                               = each.value.min_capacity
-  create_mode                                                = each.value.create_mode
+  create_mode                                                = each.value.create_mode != null && each.value.import == null ? each.value.create_mode : null
   license_type                                               = each.value.license_type
   ledger_enabled                                             = each.value.ledger_enabled
   geo_backup_enabled                                         = each.value.geo_backup_enabled
@@ -115,11 +119,41 @@ resource "azurerm_mssql_database" "database" {
   transparent_data_encryption_key_automatic_rotation_enabled = each.value.transparent_data_encryption_key_automatic_rotation_enabled
   maintenance_configuration_name                             = each.value.maintenance_configuration_name
   recover_database_id                                        = each.value.recover_database_id
+  recovery_point_id                                          = each.value.recovery_point_id
   restore_point_in_time                                      = each.value.restore_point_in_time
   auto_pause_delay_in_minutes                                = each.value.auto_pause_delay_in_minutes
   creation_source_database_id                                = each.value.creation_source_database_id
   restore_dropped_database_id                                = each.value.restore_dropped_database_id
+  secondary_type                                             = each.value.secondary_type
   tags                                                       = each.value.tags
+
+  dynamic "import" {
+    for_each = each.value.import != null ? [each.value.import] : []
+
+    content {
+      storage_uri                  = import.value.storage_uri
+      storage_key                  = import.value.storage_key
+      storage_key_type             = import.value.storage_key_type
+      administrator_login          = import.value.administrator_login
+      administrator_login_password = import.value.administrator_login_password
+      authentication_type          = import.value.authentication_type
+      storage_account_id           = try(import.value.storage_account_id, null)
+    }
+  }
+
+  dynamic "threat_detection_policy" {
+    for_each = each.value.threat_detection_policy != null ? [each.value.threat_detection_policy] : []
+
+    content {
+      state                      = try(threat_detection_policy.value.state, "Disabled")
+      disabled_alerts            = try(threat_detection_policy.value.disabled_alerts, null)
+      email_account_admins       = try(threat_detection_policy.value.email_account_admins, "Disabled")
+      email_addresses            = try(threat_detection_policy.value.email_addresses, null)
+      retention_days             = try(threat_detection_policy.value.retention_days, null)
+      storage_account_access_key = try(threat_detection_policy.value.storage_account_access_key, null)
+      storage_endpoint           = try(threat_detection_policy.value.storage_endpoint, null)
+    }
+  }
 
   dynamic "long_term_retention_policy" {
     for_each = each.value.long_term_retention_policy != null ? [each.value.long_term_retention_policy] : []
@@ -138,6 +172,15 @@ resource "azurerm_mssql_database" "database" {
     content {
       retention_days           = try(short_term_retention_policy.value.retention_days, null)
       backup_interval_in_hours = try(short_term_retention_policy.value.backup_interval_in_hours, 12)
+    }
+  }
+
+  dynamic "identity" {
+    for_each = each.value.identity != null ? [each.value.identity] : []
+
+    content {
+      type         = "UserAssigned"
+      identity_ids = identity.value.identity_ids
     }
   }
 }
