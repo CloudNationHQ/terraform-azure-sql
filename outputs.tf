ca2b226921fed86817b3369aa8efd744a6d2d0b6
dkooll <dennis.kool@cloudnation.nl>
2026-04-03 08:55:56 +0200

    feat: add missing outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "databases" {
   description = "contains databases"
   value       = azurerm_mssql_database.database
 }
+
+output "tde" {
+  description = "contains transparent data encryption details"
+  value       = azurerm_mssql_server_transparent_data_encryption.tde
+}
+
+output "network_rules" {
+  description = "contains virtual network rules"
+  value       = azurerm_mssql_virtual_network_rule.vnetrule
+}
+
+output "fw_rules" {
+  description = "contains firewall rules"
+  value       = azurerm_mssql_firewall_rule.firewallrule
+}
